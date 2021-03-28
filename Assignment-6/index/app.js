610e9fb3437c3d3c46b86d50ee69835cb54ab630
arifmamon <arifmamon222@gmail.com>
2021-03-29 00:10:54 +0530

    .

--- Assignment-6/index/app.js
+++ Assignment-6/index/app.js
@@ -4,20 +4,31 @@ const sholist = document.getElementById('showlist');
 const todolist = document.querySelector('.list');
 const clear = document.querySelector('.clear');
 
+const deleteTask = function() {
+	console.log("Delete task...");
+	var listItem = this.parentNode;
+	var ul = listItem.parentNode;
+
+	//Remove the parent list item from the ul
+	ul.removeChild(listItem);
+}
 
 button.onclick = function(e) {
 
     const val = input.value;
     const list = document.createElement('li');
 
-    const delbtn = document.createElement("close");
-    // delbtn.className = "close";
-    
+    const delbtn = document.createElement("button");
+    delbtn.innerText = "Delete";
+    delbtn.className = "delete";
+    list.appendChild(delbtn);
+
+    delbtn.onclick = deleteTask;
     list.innerText = val;
-    list.onclick = function (e){
-        console.log(e);
-        e.target.remove();
-    }
+    // list.onclick = function (e){
+    //     console.log(e);
+    //     e.target.remove();
+    // }
     sholist.append(list);
 
 
