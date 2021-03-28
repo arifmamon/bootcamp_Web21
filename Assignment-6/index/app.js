f47ac578ac09088798ab32f067cf5b9220a59676
arifmamon <arifmamon222@gmail.com>
2021-03-28 23:15:07 +0530

    .

--- Assignment-6/index/app.js
+++ Assignment-6/index/app.js
@@ -1,12 +1,24 @@
 const input = document.getElementById('input_type');
 const button = document.getElementById('button');
 const sholist = document.getElementById('showlist');
+const todolist = document.querySelector('.list');
+const clear = document.querySelector('.clear');
+
 
 button.onclick = function(e) {
 
     const val = input.value;
     const list = document.createElement('li');
-    list.innerText = val;
 
+    const delbtn = document.createElement("close");
+    // delbtn.className = "close";
+    
+    list.innerText = val;
+    list.onclick = function (e){
+        console.log(e);
+        e.target.remove();
+    }
     sholist.append(list);
+
+
 }
