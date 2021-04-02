f66ed1d77a4c9b50195f89f98d44a41e35d4edb2
arifmamon <arifmamon222@gmail.com>
2021-04-02 12:13:32 +0530

    .

--- ASsignment-7/app.js
+++ ASsignment-7/app.js
@@ -1,9 +1,23 @@
 
 const input = document.getElementById('form');
-
+const cityName = document.querySelector('.cityName');
+const countryName = document.querySelector('.countryName');
+const showWeather = document.querySelector('.weather');
+const weIcon  = document.querySelector('.icon');
 
 const getWeather = async(searchtext) => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=$searchtext&APPID=0c93f81cd20966730205c9b8a1277096`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${searchtext}&APPID=0c93f81cd20966730205c9b8a1277096`)
+    .then(res => res.json())
+    .then(data => {
+        var namevalue = data['name'];
+        var country = data['sys']['country'];
+        var weather = data['weather']['0']['description'];
+        
+        cityName.innerHTML = namevalue;
+        countryName.innerHTML = country;
+        showWeather.innerHTML = weather;
+
+    })
 }
 input.addEventListener('submit', (e) => {
     console.dir(input.elements[0].value);
