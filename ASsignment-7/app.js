6f1851a5a56783921d9b1ae56a4d92911f47906a
arifmamon <arifmamon222@gmail.com>
2021-04-02 10:01:50 +0530

    api

--- ASsignment-7/app.js
+++ ASsignment-7/app.js
@@ -2,16 +2,12 @@
 const input = document.getElementById('form');
 
 
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=0c93f81cd20966730205c9b8a1277096`)
-    .then(data => {
-        console.log(data);
-        return data.json();
-    })
-    .then(res => console.log(res))
-
+const getWeather = async(searchtext) => {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=$searchtext&APPID=0c93f81cd20966730205c9b8a1277096`)
+}
 input.addEventListener('submit', (e) => {
     console.dir(input.elements[0].value);
-
+    const searchtext = input.elements[0].value;
+    getWeather(searchtext);
     e.preventDefault();
 })
